1a41bd3d5547bf33972b252653b8214f7a26793d
Arya Kaujalgi <aryakaujalgi@gmail.com>
2023-12-14 14:44:35 +0530

    index.js uri

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:3000'
+export const API_BASE_URL = 'http://localhost:8080'
 export const ACCESS_TOKEN = 'accessToken'
 
 export const OAUTH2_REDIRECT_URI = 'https://referio.netlify.app/oauth2/redirect'
